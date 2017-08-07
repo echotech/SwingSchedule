36d762f9432a75ffec07767a7efa770cad4f5414
jreisner <7bresx8>
2017-08-07 14:57:29 -0600

    Fixing EditCustomerPanel

--- src/schedulingapplication/view/EditCustomerPanel.java
+++ src/schedulingapplication/view/EditCustomerPanel.java
@@ -24,74 +24,74 @@ import schedulingapplication.dao.TestConnection;
  *
  * @author jreisner
  */
-public class EditCustomerPanel extends JFrame{
-    
-      /**
+public class EditCustomerPanel extends JFrame {
+
+    /**
      * Creates new form CustomerForm
      */
     public EditCustomerPanel(String title) {
         super(title);
-        try{
-        initComponents();
-        
-        }catch (Exception e){
+        try {
+            initComponents();
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            pack();
+            setLocationRelativeTo(null);
+
+        } catch (Exception e) {
             System.out.println(e);
             e.printStackTrace();
         }
-        
-        
+
     }
 
-    private void initComponents() throws Exception{
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        pack();
-        setLocationRelativeTo(null);      
-        
-    }
-    
-      
-    public static DefaultTableModel buildTableModel(ResultSet rs)
-        throws SQLException {
-
-    ResultSetMetaData metaData = rs.getMetaData();
-
-    // names of columns
-    Vector<String> columnNames = new Vector<String>();
-    int columnCount = metaData.getColumnCount();
-    for (int column = 1; column <= columnCount; column++) {
-        columnNames.add(metaData.getColumnName(column));
-    }
-
-    // data of the table
-    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-    while (rs.next()) {
-        Vector<Object> vector = new Vector<Object>();
-        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-            vector.add(rs.getObject(columnIndex));
-        }
-        data.add(vector);
-    }
-
-    return new DefaultTableModel(data, columnNames);
-
-}
-    
-    public static void main(String[] args) throws Exception{
+    private void initComponents() throws Exception {
         try {
             Connection con = TestConnection.getConnection();
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select c.customerId, c.customerName, a.address, a.address2, a.phone, a.postalCode, ci.city\n" +
-            "from `U03q1A`.`customer` c\n" +
-            "join `U03q1A`.`address` a on c.addressId=a.addressId\n" +
-            "join `U03q1A`.`city` ci on a.cityId=ci.cityId;");
+            ResultSet rs = stmt.executeQuery("select c.customerId, c.customerName, a.address, a.address2, a.phone, a.postalCode, ci.city\n"
+                    + "from `U03q1A`.`customer` c\n"
+                    + "join `U03q1A`.`address` a on c.addressId=a.addressId\n"
+                    + "join `U03q1A`.`city` ci on a.cityId=ci.cityId;");
             JTable table = new JTable(buildTableModel(rs));
             JOptionPane.showMessageDialog(null, new JScrollPane(table));
-        }
-        catch (Exception e){
-            JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
-        }
-        
-    }
-    
-}
 
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    public static DefaultTableModel buildTableModel(ResultSet rs)
+            throws SQLException {
+
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(() -> {
+            new EditCustomerPanel(("Edit Customers")).setVisible(true);
+        });
+
+    }
+
+}
