406b60d0fbd88041fcde098089739b45a6ce7853
Jed <jed.reisner@gmail.com>
2017-08-13 23:14:17 -0600

    Code reformat.

--- src/schedulingapplication/view/EditCustomerPanel.java
+++ src/schedulingapplication/view/EditCustomerPanel.java
@@ -42,7 +42,7 @@ public class EditCustomerPanel extends JFrame {
         super(title);
         try {
             initComponents();
-            
+
             setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             pack();
             setLocationRelativeTo(null);
@@ -63,81 +63,75 @@ public class EditCustomerPanel extends JFrame {
                     + "join `U03q1A`.`address` a on c.addressId=a.addressId\n"
                     + "join `U03q1A`.`city` ci on a.cityId=ci.cityId;");
             JTable table = new JTable(buildTableModel(rs));
-            
+
             JOptionPane.showMessageDialog(null, new JScrollPane(table));
             this.jtable = table;
-            
+
             table.getModel().addTableModelListener(table);
             table.putClientProperty("terminateEditOnFocusLost", true);
-        
-        
-        
-            
-           
-                
-            } catch (Exception e) {
+
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(this, e, "Error", JOptionPane.ERROR_MESSAGE);
         }
 
     }
-    //http://docs.oracle.com/javase/tutorial/uiswing/components/table.html#modelchange
-    public void updateTable(){
-        int row = this.jtable.getSelectedRow();
-            int col = this.jtable.getSelectedColumn();
-            String sql = null;    
-            
-        try {
-            
-                if (col == 1) {
-                    sql = "Update `U03q1A`.`customer` set `customerName`=? where `customerId`=?" ;
-                    
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.executeUpdate();
 
-                }
-                if (col == 2) {
-                    sql = "Update `U03q1A`.`address` set `address`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.execute();
-                }
-                if (col == 3) {
-                    sql = "Update `U03q1A`.`address` set `address2`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.execute();
-                }
-                if (col == 4) {
-                    sql = "Update `U03q1A`.`address` set `phone`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.execute();
-                }
-                if (col == 5) {
-                    sql = "Update `U03q1A`.`address` set `postalCode`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.execute();
-                }
-                if (col == 6) {
-                    sql = "Update `U03q1A`.`city` set `city`=? where `cityId`=(select `cityID` from `U03q1A`.`address` where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?))";
-                    PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setString(1, table.getValueAt(row,col).toString());
-                    ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
-                    ps.execute();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
+    //http://docs.oracle.com/javase/tutorial/uiswing/components/table.html#modelchange
+    public void updateTable() {
+        int row = this.jtable.getSelectedRow();
+        int col = this.jtable.getSelectedColumn();
+        String sql = null;
+
+        try {
+
+            if (col == 1) {
+                sql = "Update `U03q1A`.`customer` set `customerName`=? where `customerId`=?";
+
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.executeUpdate();
+
             }
+            if (col == 2) {
+                sql = "Update `U03q1A`.`address` set `address`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.execute();
+            }
+            if (col == 3) {
+                sql = "Update `U03q1A`.`address` set `address2`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.execute();
+            }
+            if (col == 4) {
+                sql = "Update `U03q1A`.`address` set `phone`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.execute();
+            }
+            if (col == 5) {
+                sql = "Update `U03q1A`.`address` set `postalCode`=? where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.execute();
+            }
+            if (col == 6) {
+                sql = "Update `U03q1A`.`city` set `city`=? where `cityId`=(select `cityID` from `U03q1A`.`address` where `addressId`=(select `addressId` from `U03q1A`.`customer` where `customerId`=?))";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, table.getValueAt(row, col).toString());
+                ps.setInt(2, Integer.parseInt(table.getValueAt(row, 0).toString()));
+                ps.execute();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-    
-    
 
     public static DefaultTableModel buildTableModel(ResultSet rs)
             throws SQLException {
@@ -166,7 +160,7 @@ public class EditCustomerPanel extends JFrame {
     }
 
     public static void main(String[] args) throws Exception {
-        
+
         SwingUtilities.invokeLater(() -> {
             new EditCustomerPanel(("Edit Customers")).setVisible(true);
         });
